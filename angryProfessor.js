b564a4f9011cf972adf784754963352ac78bb43f
ThiagoDeiro <thiagodeiro.11@hotmail.com>
2020-03-30 14:21:55 +1100

    first commit

---
+++ angryProfessor.js
@@ -0,0 +1,16 @@
+function angryProfessor(k, a) {
+  let count = 0;
+
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] <= 0) {
+      count++;
+    }
+  }
+  if (count === k || count > k) {
+    return "NO";
+  } else {
+    return "YES";
+  }
+}
+
+console.log(angryProfessor(3, [-1, -3, 4, 2]));
